80a8fce3f53d2099fb73d76124f3a643aa7558b1
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-10 13:43:38 +0100

    Feat: This add the auth.js and connect it to app.js file

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const authRouter = require("./routes/auth");
 
 dotenv.config();
 
 async function connect() {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to the database");
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to the database");
+  } catch (err) {
+    console.log(err);
+  }
 }
 connect();
 
-const app = express();
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
+app.use("/api/auth", authRouter);
+
+// port = process.env.MONGODB_URI || 3000;
 app.listen(3000, () => {
-    console.log("Server is running on port 3000...");
+  console.log("Server is running on port 3000...");
 });
