040d3e504b9be6fdcde7004e255a048da3e52f04
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-12 19:42:13 +0100

    Feat: This add users unfollow implementation

--- routes/users.js
+++ routes/users.js
@@ -75,16 +75,39 @@ router.put("/:id/follow", async (req, res) => {
       if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({ $push: { followings: req.body.userId } });
-        res.status(200).json("User has been followed");
+        res.status(200).json("User has been followed!");
       } else {
-        res.status(403).json("You already followed this user");
+        res.status(403).json("You already followed this user!");
       }
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You can not follow yourself");
+    res.status(403).json("You can not follow yourself!");
   }
 });
+
+// Follow a user
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        const currentUser = await User.findById(req.body.userId);
+  
+        if (!user.followers.includes(req.body.userId)) {
+          await user.updateOne({ $pull: { followers: req.body.userId } });
+          await currentUser.updateOne({ $pull: { followings: req.body.userId } });
+          res.status(200).json("User has been unfollowed!");
+        } else {
+          res.status(403).json("You already unfollowed this user!");
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You can not unfollow yourself!");
+    }
+  });
+  
 
 module.exports = router;
