619dd421f51151167e37d9bb35836aa780ba442d
lotacodic <ngenejoshua05@gmail.com>
2026-06-27 09:40:02 +0100

    feat(controllers): add auth, user, post, and comment controllers

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
- const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
 const userRouter = require("./routes/users");
 
-dotenv.config();
+const app = express();
 
-async function connect() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to the database");
-  } catch (err) {
-    console.log(err);
-  }
-}
-connect();
-
-// middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"))
+app.use(morgan("common"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
 
-// port = process.env.MONGODB_URI || 3000;
-app.listen(3000, () => {
-  console.log("Server is running on port 3000...");
-});
+module.exports = app;
